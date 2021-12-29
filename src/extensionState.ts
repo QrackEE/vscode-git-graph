03f1fddc0abb824f619ac504c61404be789c8b6c
cweijan <cweijan@163.com>
2021-12-29 17:53:43 +0800

    Always accept checkout and change default reset mode.

--- src/extensionState.ts
+++ src/extensionState.ts
@@ -40,7 +40,7 @@ export const DEFAULT_REPO_STATE: GitRepoState = {
 };
 
 const DEFAULT_GIT_GRAPH_VIEW_GLOBAL_STATE: GitGraphViewGlobalState = {
-	alwaysAcceptCheckoutCommit: false,
+	alwaysAcceptCheckoutCommit: true,
 	issueLinkingConfig: null,
 	pushTagSkipRemoteCheck: false
 };
